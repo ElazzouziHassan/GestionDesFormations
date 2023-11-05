541cc9978e2db2c6c5e43224c1d14d60d624251c
wizardy <wizardhassan99@gmail.com>
2023-11-05 22:16:52 +0100

    Création de la table "Administrateur"

--- Partie1_Conception/getsion_formation_db.sql
+++ Partie1_Conception/getsion_formation_db.sql
@@ -4,4 +4,13 @@ CREATE DATABASE gestion_formation_db;
 -- Utilisation de la base de données
 USE gestion_formation_db;
 
-
+-- Création de la table "Administrateur"
+CREATE TABLE Administrateur (
+    Identifiant INT AUTO_INCREMENT PRIMARY KEY,
+    NumeroCarteIdentite VARCHAR(20),
+    Nom VARCHAR(50),
+    DateNaissance DATE,
+    NumeroTelephone VARCHAR(20),
+    AdresseEmail VARCHAR(100),
+    Adresse VARCHAR(255)
+);
