1c56e51bb83dca477036bb656ceddbd83a855a44
wizardy <wizardhassan99@gmail.com>
2023-11-06 22:22:03 +0100

    etudiant model

--- Partie2_App/app/Models/etudiantModel.php
+++ Partie2_App/app/Models/etudiantModel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class etudiantModel extends Model
 {
     use HasFactory;
+    protected $fillable = ['cin', 'nom', 'date_naissance', 'adresse', 'adresse_email', 'numero_telephone'];
+
+    public function formations() {
+        return $this->belongsToMany('formation');
+    }
 }
