bd046f33972dc4df9fd74812d06ae052c0b5fe4c
wizardy <wizardhassan99@gmail.com>
2023-11-06 21:52:14 +0100

    admin model controller

---
+++ Partie2_App/app/Http/Controllers/AdminModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\adminModel;
+use Illuminate\Http\Request;
+
+class AdminModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(adminModel $adminModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(adminModel $adminModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, adminModel $adminModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(adminModel $adminModel)
+    {
+        //
+    }
+}
