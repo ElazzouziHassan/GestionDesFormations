9ad5fb7dcb89894bb9318774f12b46349cde4868
wizardy <wizardhassan99@gmail.com>
2023-11-06 21:52:02 +0100

    etudiant model controller

---
+++ Partie2_App/app/Http/Controllers/EtudiantModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\etudiantModel;
+use Illuminate\Http\Request;
+
+class EtudiantModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(etudiantModel $etudiantModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(etudiantModel $etudiantModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, etudiantModel $etudiantModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(etudiantModel $etudiantModel)
+    {
+        //
+    }
+}
