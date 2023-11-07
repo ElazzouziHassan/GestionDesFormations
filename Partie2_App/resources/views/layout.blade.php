04d13f763650efb40c45fe43939912873c8163a6
wizardy <wizardhassan99@gmail.com>
2023-11-07 21:13:38 +0100

    layout

---
+++ Partie2_App/resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> Gestion Formation | @yield('title')</title>
+</head>
+<body>
+  @yield('navbar');
+  @yield('header');
+  @yield('content');
+  @yield('temoignages');
+  @yield('footer');
+</body>
+</html>
