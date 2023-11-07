16dc57929dd13c7a6e448b6d6be0f30640b7cbc2
wizardy <wizardhassan99@gmail.com>
2023-11-07 20:57:10 +0100

    commit

---
+++ Partie1_Conception/Q3-gestion_formation_db.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2023 at 09:44 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gestion_formation_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrateur`
+--
+
+CREATE TABLE `administrateur` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `adresse_email` varchar(255) NOT NULL,
+  `numero_telephone` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etudiant`
+--
+
+CREATE TABLE `etudiant` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `adresse_email` varchar(255) NOT NULL,
+  `numero_telephone` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etudiant_formation`
+--
+
+CREATE TABLE `etudiant_formation` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `formation_id` bigint(20) UNSIGNED NOT NULL,
+  `etudiant_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formateur`
+--
+
+CREATE TABLE `formateur` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `adresse_email` varchar(255) NOT NULL,
+  `numero_telephone` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formation`
+--
+
+CREATE TABLE `formation` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `date_debut` date NOT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `duree_en_mois` int(11) NOT NULL,
+  `intitule` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `administrateur_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formation_formateur`
+--
+
+CREATE TABLE `formation_formateur` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `formation_id` bigint(20) UNSIGNED NOT NULL,
+  `formateur_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrateur`
+--
+ALTER TABLE `administrateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `etudiant_formation`
+--
+ALTER TABLE `etudiant_formation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `etudiant_formation_etudiant_id_foreign` (`etudiant_id`),
+  ADD KEY `etudiant_formation_formation_id_foreign` (`formation_id`);
+
+--
+-- Indexes for table `formateur`
+--
+ALTER TABLE `formateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `formation`
+--
+ALTER TABLE `formation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `formation_administrateur_id_foreign` (`administrateur_id`);
+
+--
+-- Indexes for table `formation_formateur`
+--
+ALTER TABLE `formation_formateur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `formation_formateur_formation_id_foreign` (`formation_id`),
+  ADD KEY `formation_formateur_formateur_id_foreign` (`formateur_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrateur`
+--
+ALTER TABLE `administrateur`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `etudiant`
+--
+ALTER TABLE `etudiant`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `etudiant_formation`
+--
+ALTER TABLE `etudiant_formation`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `formateur`
+--
+ALTER TABLE `formateur`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `formation`
+--
+ALTER TABLE `formation`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `formation_formateur`
+--
+ALTER TABLE `formation_formateur`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `etudiant_formation`
+--
+ALTER TABLE `etudiant_formation`
+  ADD CONSTRAINT `etudiant_formation_etudiant_id_foreign` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiant` (`id`),
+  ADD CONSTRAINT `etudiant_formation_formation_id_foreign` FOREIGN KEY (`formation_id`) REFERENCES `formation` (`id`);
+
+--
+-- Constraints for table `formation`
+--
+ALTER TABLE `formation`
+  ADD CONSTRAINT `formation_administrateur_id_foreign` FOREIGN KEY (`administrateur_id`) REFERENCES `administrateur` (`id`);
+
+--
+-- Constraints for table `formation_formateur`
+--
+ALTER TABLE `formation_formateur`
+  ADD CONSTRAINT `formation_formateur_formateur_id_foreign` FOREIGN KEY (`formateur_id`) REFERENCES `formateur` (`id`),
+  ADD CONSTRAINT `formation_formateur_formation_id_foreign` FOREIGN KEY (`formation_id`) REFERENCES `formation` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
