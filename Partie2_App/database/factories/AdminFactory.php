428133f5c7c2c93980e9281e11b15b3a9f200cc3
wizardy <wizardhassan99@gmail.com>
2023-11-10 22:43:10 +0100

    commit

--- Partie2_App/database/factories/AdminFactory.php
+++ Partie2_App/database/factories/AdminFactory.php
@@ -17,7 +17,12 @@ class AdminFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'cin' => strtoupper($this->faker->randomLetter().$this->faker->randomDigit(6,8)),
+            'nom' => $this->faker->name(),
+            'date_naissance' => $this->faker->date(),
+            'adresse' => $this->faker->address(),
+            'adresse_email' => $this->faker->email(),
+            'numero_telephone' => $this->faker->phoneNumber()
         ];
     }
 }
